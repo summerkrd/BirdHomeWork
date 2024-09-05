206faf950b3abf7c231f462b1a6357d767c87114
summerkrd <145993794+summerkrd@users.noreply.github.com>
2024-09-05 16:32:03 +0300

    TheEnd

--- Assets/Scripts/CharacterFlyControl.cs
+++ Assets/Scripts/CharacterFlyControl.cs
@@ -1,10 +1,14 @@
 using System.Collections;
+using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class CharacterFlyControl : MonoBehaviour
 {
     [SerializeField] private SoundManager _soundManager;
-    [SerializeField] private Animator _animator;
+    [SerializeField] private Animator[] _animators;  
+    [SerializeField] private List<GameObject> _characterSkin;
+    [SerializeField] private TextMeshProUGUI _jumpCountText;
 
     [SerializeField] private int _forceValueForUp;
     [SerializeField] private float _forceValueForLeftRight;    
@@ -12,24 +16,26 @@ public class CharacterFlyControl : MonoBehaviour
     private Rigidbody _rigidbody;
     private int _playerRotationAngleLimit = 30;
     private string _jumpAnimation = "Jump";
-
+    private int _currentCharacter;
+    
     public int JumpCount { get; private set; }    
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody>();
+        
+        FindCurrentCharacterID();
     }
 
     void Update()
     {        
         PlayerRotation();
 
+        _jumpCountText.text = $"Количество прыжков: {JumpCount.ToString()}";
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            PlayerMove(Vector3.up, _forceValueForUp, ForceMode.Impulse);
-            _soundManager.PlayFlapSound();
-            _animator.SetTrigger(_jumpAnimation);
-            JumpCount++;
+            PlayerJupm();
         }
         else if (Input.GetKey(KeyCode.A))
         {
@@ -48,12 +54,46 @@ public class CharacterFlyControl : MonoBehaviour
 
     public void ResetVelocity()
     {
-        _rigidbody.velocity = Vector3.zero;        
+        _rigidbody.velocity = Vector3.zero;
+    }
+
+    private void FindCurrentCharacterID()
+    {
+        foreach (GameObject character in _characterSkin)
+        {
+            if (character.activeSelf)
+            {
+                _currentCharacter = _characterSkin.IndexOf(character);
+            }
+        }
+    }
+
+    public void SwitchCharacter()
+    {
+        FindCurrentCharacterID();
+        _characterSkin[_currentCharacter].SetActive(false);
+        _characterSkin[Random.Range(0, _characterSkin.Count)].SetActive(true);
     }
 
     private void PlayerMove(Vector3 moveDirection, float forceValue, ForceMode forceMode)
     {
         _rigidbody.AddForce(moveDirection * forceValue, forceMode);
+    }
+
+    private void PlayerJupm()
+    {
+        PlayerMove(Vector3.up, _forceValueForUp, ForceMode.Impulse);
+        _soundManager.PlayFlapSound();
+
+        foreach (Animator animator in _animators)
+        {
+            if (animator.gameObject.activeSelf)
+            {
+                animator.SetTrigger(_jumpAnimation);
+            }
+        }
+
+        JumpCount++;
     }
 
     private void PlayerRotation()
--- Assets/Scripts/GameTracking.cs
+++ Assets/Scripts/GameTracking.cs
@@ -9,9 +9,13 @@ public class GameTracking : MonoBehaviour
     [SerializeField] private float _leftAndRightLimit;
     
     [SerializeField] private Transform _upperObstacle;
-    [SerializeField] private Transform _lowerObstacle;    
+    [SerializeField] private Transform _lowerObstacle;
+
+    [SerializeField] private Animator _CameraAnimator;
+    [SerializeField] private ParticleSystem _explotion;
     
     private Vector3 _playerPosition;
+    private string _cameraShakeAnimation = "Shake";
 
     private void Start()
     {
@@ -29,7 +33,8 @@ public class GameTracking : MonoBehaviour
         {            
             PlayerDie();            
         }
-        else if (_playerPosition.x > _leftAndRightLimit)
+        
+        if (_playerPosition.x > _leftAndRightLimit)
         {
             _playerPosition.x = _leftAndRightLimit;
         }
@@ -49,16 +54,26 @@ public class GameTracking : MonoBehaviour
     private void StartGame()
     {
         _soundManager.PlayBackgroundSound(true);        
+        
         _characterFlyControl.ResetVelocity();
         _characterFlyControl.gameObject.SetActive(true);   
         _characterFlyControl.ResetJumpCounter();
+        _characterFlyControl.SwitchCharacter();
+       
         StartCoroutine(_characterFlyControl.JumpOnStart());
     }
 
     private void PlayerDie()
     {
-        _playerPosition = Vector3.zero;
+        _CameraAnimator.SetTrigger(_cameraShakeAnimation);             
+        
         _characterFlyControl.gameObject.SetActive(false);
+
+        _explotion.transform.position = _playerPosition;
+        _explotion.Play();
+
+        _playerPosition = Vector3.zero;
+
         _soundManager.PlayHitSound();
         _soundManager.PlayBackgroundSound(false);
     }
